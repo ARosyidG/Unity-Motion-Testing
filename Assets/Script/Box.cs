d892ca46e7931be3cf5a9325bf9b6b6f1d585fec
ARosyidG <RosyidKeren164@gmail.com>
2023-09-12 16:39:04 +0800

    translate feature

--- Assets/Script/Box.cs
+++ Assets/Script/Box.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.UIElements;
+
 
 public class Box : MonoBehaviour
 {
@@ -16,17 +15,31 @@ public class Box : MonoBehaviour
         Vector3 cameraX = Camera.main.transform.right;
         Vector3 cameraY = Camera.main.transform.up;
         Vector3 cameraRelativeX = Input.GetAxis("Mouse Y") * cameraX;
-        Vector3 cameraRelativeY = Input.GetAxis("Mouse X")*(-1)* cameraY;
-        Vector3 controllerMotionInput = (cameraRelativeX + cameraRelativeY).normalized;
+        Vector3 cameraRelativeY = Input.GetAxis("Mouse X") * (-1) * cameraY;
+        Vector3 controllerMotionInput = (cameraRelativeX + cameraRelativeY).normalized; 
         print(controllerMotionInput);
         this.transform.Rotate(controllerMotionInput * 300 * Time.deltaTime,Space.World);
     }
+    bool translateInputToggle = false;
     void translateModel(){
-
+        if(Input.GetKeyDown(KeyCode.T)){
+            translateInputToggle = !translateInputToggle;
+            print(translateInputToggle);
+        }
+        if(translateInputToggle){
+            Vector3 cameraX = Camera.main.transform.right;
+            Vector3 cameraY = Camera.main.transform.up;
+            Vector3 cameraRelativeX = Input.GetAxis("Mouse X") * cameraX;
+            Vector3 cameraRelativeY = Input.GetAxis("Mouse Y") * cameraY;
+            Vector3 controllerMotionInput = (cameraRelativeX + cameraRelativeY).normalized; 
+            print(controllerMotionInput);
+            this.transform.Translate(controllerMotionInput * 25 * Time.deltaTime,Space.World);
+        }
     }
 
     void Update()
     {
-        rotateModel();
+        // rotateModel();
+        translateModel();
     }
 }
